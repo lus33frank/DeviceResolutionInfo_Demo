cd58ebc4886ea2511957d5fd2b10bed06b8a4851
lus33frank <lus33frank@gmail.com>
2018-10-23 22:12:24 +0800

    Commit : 2    專案練習完成。 裝置解析度資訊（螢幕）

--- app/src/main/java/com/frankchang/deviceresolutioninfo_demo/MainActivity.java
+++ app/src/main/java/com/frankchang/deviceresolutioninfo_demo/MainActivity.java
@@ -2,12 +2,32 @@ package com.frankchang.deviceresolutioninfo_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvShow;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvShow = findViewById(R.id.tvShow);
     }
+
+    public void getInfoOnClick(View view) {
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        int dpi = displayMetrics.densityDpi;
+
+        String info = "Height : " + height + "\nWidth : " + width + "\nDPI : " + dpi;
+        tvShow.setText(info);
+    }
+
 }
